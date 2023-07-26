c26d48a8b9dc2e3fa8027e14080c9c82c9c33b8c
princekalra <kalraprince75@gmail.com>
2023-07-26 17:52:59 +0530

    Implemented complex div structure by using React

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const Heading = React.createElement("h1", {'id':'heading'}, "Hello World ! from React");
-console.log(Heading); // object
+/**
+ *
+ * <div id="parent">
+ * <div id="child1">
+ * <h1>Heading 1 from Child1!!</h1>
+ * <h2>Heading 2 from Child1!!</h2>
+ * </div>
+ * <div id="child2">
+ * <h1>Heading 1 from Child2!!</h1>
+ *  <h2>Heading 2 from Child2!!</h2>
+ * </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement("div", { id: "Parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1" },
+
+    [
+      React.createElement("h1", null, "Heading  1 from Child1!!"),
+      React.createElement("h2", null, "Heading  2 from Child1!!"),
+    ]
+  ),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", null, "Heading 1 from Child2!!"),
+    React.createElement("h2", null, "Heading 2 from Child2!!"),
+  ]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root); //object
-root.render(Heading);
+root.render(parent);
