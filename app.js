23e4e03a4870f3ca3521720cbd50021ca9490aae
princekalra <kalraprince75@gmail.com>
2023-09-09 00:43:25 +0530

    started using jsx

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-/**
- *
- * <div id="parent">
- * <div id="child1">
- * <h1>Heading 1 from Child1!!</h1>
- * <h2>Heading 2 from Child1!!</h2>
- * </div>
- * <div id="child2">
- * <h1>Heading 1 from Child2!!</h1>
- *  <h2>Heading 2 from Child2!!</h2>
- * </div>
- * </div>
- *
- */
 import React from "react";
-import ReactDOM  from "react-dom";
-const parent = React.createElement("div", { id: "Parent" }, [
-  React.createElement(
-    "div",
-    { id: "child1" },
+import ReactDOM from "react-dom";
 
-    [
-      React.createElement("h1", null, "Heading  1 from Child1!!"),
-      React.createElement("h2", null, "Heading  2 from Child1!!"),
-    ]
-  ),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", null, "Heading 1 from Child2!!"),
-    React.createElement("h2", null, "Heading 2 from Child2!!"),
-  ]),
-]);
+const heading = React.createElement(
+  "h1",
+  { id: " heading" },
+  "This is the heading"
+);
+//Babel Transpiles JSX to React. Create Element(JavaScript Object) => HTML Element while Rendering
+const jsxHeading = (
+  <h1 className="head" tabIndex="5">
+    Heading By using JSX
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
+
+// Firstly create Element hme object deta ha => fr hm us object ko DOM p render krate to vo DOM Element hojata
